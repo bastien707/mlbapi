e5bacc1dd92823b34db54b161a2a1f561a8eb0b3
Sl33pyCl0ud <72408263+Sl33pyCl0ud@users.noreply.github.com>
2023-07-04 15:00:53 +0200

    Starter

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,5 +1,51 @@
-@main def hello: Unit =
-  println("Hello world!")
-  println(msg)
+package mlb
 
-def msg = "I was compiled by Scala 3. :)"
+import zio._
+import zio.jdbc._
+import zio.http._
+
+object MlbApi extends ZIOAppDefault {
+
+  val createZIOPoolConfig: ULayer[ZConnectionPoolConfig] =
+    ZLayer.succeed(ZConnectionPoolConfig.default)
+
+  val properties: Map[String, String] = Map(
+    "user" -> "postgres",
+    "password" -> "postgres"
+  )
+
+  val connectionPool : ZLayer[ZConnectionPoolConfig, Throwable, ZConnectionPool] =
+    ZConnectionPool.h2mem(
+      database = "testdb",
+      props = properties
+    )
+
+  val create: ZIO[ZConnectionPool, Throwable, Unit] = transaction {
+    execute(
+      sql"CREATE TABLE IF NOT EXISTS ..."
+    )
+  }
+
+  val insertRows: ZIO[ZConnectionPool, Throwable, UpdateResult] = transaction {
+    insert(
+      sql"INSERT INTO ...".values((value1, value2, value2))
+    )
+  }
+
+  val endpoints: App[Any] =
+    Http
+      .collect[Request] {
+        case Method.GET -> Root / "init" => ???
+        case Method.GET -> Root / "games" => ???
+        case Method.GET -> Root / "predict" / "game" / gameId => ???
+      }
+      .withDefaultErrorResponse
+
+  val app: ZIO[ZConnectionPool & Server, Throwable, Unit] = for {
+    conn <- create *> insertRows
+    _ <- Server.serve(endpoints)
+  } yield ()
+
+  override def run: ZIO[Any, Throwable, Unit] =
+    app.provide(createZIOPoolConfig >>> connectionPool, Server.default)
+}
