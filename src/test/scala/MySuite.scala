6fdf9d97e01b22b85ae3999c6a053ab55fbec0bb
Jonathan <100156632+Assowavesss@users.noreply.github.com>
2023-07-20 18:36:51 +0200

    change value test suite

--- src/test/scala/MySuite.scala
+++ src/test/scala/MySuite.scala
@@ -2,8 +2,8 @@
 // https://scalameta.org/munit/docs/getting-started.html
 class MySuite extends munit.FunSuite {
   test("example test that succeeds") {
-    val obtained = 42
-    val expected = 42
+    val obtained = 43
+    val expected = 43
     assertEquals(obtained, expected)
   }
 }
